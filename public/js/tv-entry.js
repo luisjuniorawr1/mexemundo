ea145a2f8ae85a66d43b8f8f7b8bbaf91cbcb47d
luisjuniorawr1 <luis.junior.lima@gmail.com>
2026-08-05 23:21:27 -0300

    Exige duas mãos e ativa interface universal no Estoura-Balões

--- public/js/tv-entry.js
+++ public/js/tv-entry.js
@@ -1,4 +1,8 @@
-import { installSingleHandGameActivation } from './single-hand-game-activation.js';
+import {
+  installUniversalGameHandUI,
+  startUniversalGameHandUI
+} from './universal-game-hand-ui.js';
 
-installSingleHandGameActivation();
+installUniversalGameHandUI();
 await import('./tv.js');
+startUniversalGameHandUI();
